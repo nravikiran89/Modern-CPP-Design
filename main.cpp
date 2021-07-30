416474c7dab274d41c0ae17d9c8837a7541e9a0b
Ravi Kiran Nagaraj <ravikiran89@outlook.com>
2021-07-30 12:52:11 +0530

    Updated to call power and factorials.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "math/powers.hpp"
+#include "math/factorials.hpp"
 
 int main() {
 
-    std::cout << power<7, 5>::value << std::endl;
+    std::cout << power<11U, 2U>::value << std::endl;
+    std::cout << factorial<11U>::value << std::endl;
 
     return 0;
 }
