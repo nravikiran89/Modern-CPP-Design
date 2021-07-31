1b2f8c9474087afe4b05054f5100c5f62fbdfcd7
Ravi Kiran Nagaraj <ravikiran89@outlook.com>
2021-07-31 19:37:09 +0530

    Updates with new template specialization and namespaces.

--- math/powers.hpp
+++ math/powers.hpp
@@ -1,30 +1,60 @@
 #ifndef __MATH_POWERS_HPP__
 #define __MATH_POWERS_HPP__
 
-/**
- * @brief Structure to compute powers of an integer value.
- *        Computes (BASE ** Exponent) during compile time.
- * 
- * @tparam BASE         - Base value
- * @tparam EXPONENT     - Integer Value
- */
-template<unsigned int BASE, unsigned int EXPONENT>
-struct power
+namespace custom
 {
-    enum { value = BASE * power<BASE, EXPONENT - 1U>::value };
-};
+    namespace math
+    {
+        /**
+         * @brief Structure to compute powers of an integer value.
+         *        Computes (BASE ** Exponent) during compile time.
+         * 
+         * @tparam BASE         - Base value
+         * @tparam EXPONENT     - Integer Value
+         */
+        template<unsigned int BASE, unsigned int EXPONENT>
+        struct power
+        {
+            enum { value = BASE * power<BASE, EXPONENT - 1U>::value };
+        };
 
-/**
- * @brief Partial template specialization of power structure.
- *        Return 1 if the exponent passed is 0
- * 
- * @tparam BASE         - Base value
- */
-template<unsigned int BASE>
-struct power<BASE, 0U>
-{
-    enum { value = 1U };
-};
+        /**
+         * @brief Partial template specialization of power structure.
+         *        Return 1 if the exponent passed is 0
+         * 
+         * @tparam BASE         - Base value
+         */
+        template<unsigned int BASE>
+        struct power<BASE, 0U>
+        {
+            enum { value = 1U };
+        };
+
+        /**
+         * @brief Partial template specialization of power structure.
+         *        Return 1 if the base passed is 0
+         * 
+         * @tparam BASE         - Base value
+         */
+        template<unsigned int EXPONENT>
+        struct power<0U, EXPONENT>
+        {
+            enum { value = 1U };
+        };
+
+        /**
+         * @brief Partial template specialization of power structure.
+         *        Return 1 if the base passed is 1
+         * 
+         * @tparam BASE         - Base value
+         */
+        template<unsigned int EXPONENT>
+        struct power<1U, EXPONENT>
+        {
+            enum { value = 1U };
+        };
+    }
+}
 
 
 #endif // !__MATH_POWERS_HPP__
