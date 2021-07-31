e94613f8b647541be099d4964110ff7b59897e50
Ravi Kiran Nagaraj <ravikiran89@outlook.com>
2021-07-31 17:07:00 +0530

    Default NullType class. Has nothing and does nothing.

---
+++ frame/NullType.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file NullType.hpp
+ * @author Ravi Kiran Nagaraj (ravikiran89@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-31
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#ifndef __NULL_TYPE_HPP__
+#define __NULL_TYPE_HPP__
+
+struct NullType {  };
+
+
+#endif /* __NULL_TYPE_HPP__ */
