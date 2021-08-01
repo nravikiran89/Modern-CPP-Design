04ce10c05dd64e45b93e55a05ed24966a54c9c1a
Ravi Kiran Nagaraj <ravikiran89@outlook.com>
2021-08-01 09:27:41 +0530

    Adding Length functionality for Typelist

--- frame/typelists.hpp
+++ frame/typelists.hpp
@@ -13,6 +13,18 @@
 #ifndef __TYPELISTS_HPP__
 #define __TYPELISTS_HPP__
 
+#include "NullType.hpp"
+
+/**
+ * @brief Preprocessor defines to wrap around Typelist structure
+ * 
+ */
+#define TYPELIST_1(T1)              Typelist<T1, NullType>
+#define TYPELIST_2(T1, T2)          Typelist<T1, TYPELIST_1(T2) >
+#define TYPELIST_3(T1, T2, T3)      Typelist<T1, TYPELIST_2(T2, T3) >
+#define TYPELIST_4(T1, T2, T3, T4)  Typelist<T1, TYPELIST_3(T2, T3, T4) >
+// .. and so on
+
 /**
  * @brief Base structure to define Typelists
  * 
@@ -32,7 +44,38 @@ struct Typelist
  */
 namespace tl
 {
+    namespace length
+    {
+        /**
+         * @brief Declaration for computing length of Typelist
+         * 
+         * @tparam LIST Input Typelist struct
+         */
+        template <typename LIST> struct Length;
 
+        /**
+         * @brief Template specialization for Length is NullType whose value is 0
+         * 
+         * @tparam  NullType
+         */
+        template <>
+        struct Length<NullType>
+        {
+            enum { value = 0 };
+        };
+        
+        /**
+         * @brief Definition for computing length of Typelist
+         * 
+         * @tparam H 
+         * @tparam T 
+         */
+        template <typename H, typename T>
+        struct Length < Typelist<H, T> >
+        {
+            enum { value = 1 + Length<T>::value };
+        };
+    }
 }
 
 #endif /* __TYPELISTS_HPP__ */
